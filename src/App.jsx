c52153f3bffc594a98efaeeaafa167952d2eb37c
Marcelo <mjfcamspo@gmail.com>
2023-05-16 16:59:38 -0700

    Add header html and css

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,35 @@
-import logo from "./logo.svg";
+import logo from "./assets/logo/BrainFlix-logo.svg";
+import search from "./assets/icons/search.svg";
+import avatar from "./assets/images/Mohan-muruge.jpg";
 import "./styles/global.scss";
-import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+      <header class="header">
+        <a href="./index.html" class="header__logo">
+          <img className="header__logo-img" src={logo} alt="logo" />
         </a>
+        <div className="header__search">
+          <form className="header__search-form">
+            <button className="header__search-form-btn">
+              <img src={search} alt="search" />
+            </button>
+            <input
+              type="text"
+              className="header__search-form-input"
+              name="search"
+              placeholder="Search"
+            />
+            <div className="header__search-avatar">
+              <img
+                src={avatar}
+                alt="avatar"
+                className="header__search-avatar-img"
+              />
+            </div>
+          </form>
+        </div>
       </header>
     </div>
   );
