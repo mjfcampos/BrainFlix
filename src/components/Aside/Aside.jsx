c7f3738a5a7aabc027b97cd642b800be58489fc4
Marcelo <mjfcamspo@gmail.com>
2023-05-21 11:47:05 -0700

    Add style for Aside component Change style for Hero component Add style for VideoList component

--- src/components/Aside/Aside.jsx
+++ src/components/Aside/Aside.jsx
@@ -3,9 +3,9 @@ import "./Aside.scss";
 
 function Aside({ videos, activeVideoDetail, handleVideoClick }) {
   return (
-    <aside className="next-videos">
-      <div className="next-videos__wrapper">
-        <h2 className="next-videos__title">Next videos</h2>
+    <aside className="videos">
+      <div className="videos__wrapper">
+        <h2 className="videos__title">Next videos</h2>
         <VideoList
           videos={videos}
           handleVideoClick={handleVideoClick}
--- src/components/VideoList/VideoList.jsx
+++ src/components/VideoList/VideoList.jsx
@@ -2,21 +2,29 @@ import "./VideoList.scss";
 
 function VideoList({ videos, handleVideoClick, activeVideoDetail }) {
   return (
-    <ul>
+    <ul className="video__list">
       {videos
         .filter((video) => video.id !== activeVideoDetail.id)
         .map((video) => (
           <li
             onClick={() => handleVideoClick(video.id)}
             key={video.id}
-            className="next-videos__container"
+            className="video__list__item"
           >
-            <div className="next-videos__video">
-              <img src={video.image} alt="" className="next-videos__video" />
+            <div className="video__list__item-video">
+              <img
+                src={video.image}
+                alt=""
+                className="video__list__item-video"
+              />
             </div>
-            <div className="next-videos__description">
-              <p className="next-videos__description-title">{video.title}</p>
-              <p className="next-videos__description-author">{video.channel}</p>
+            <div className="video__list__item-description">
+              <p className="video__list__item-description-title">
+                {video.title}
+              </p>
+              <p className="video__list__item-description-author">
+                {video.channel}
+              </p>
             </div>
           </li>
         ))}
