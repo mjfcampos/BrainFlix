66ab1d5f2c3e52fc24b0a8e2a360a7b621f28149
Marcelo <mjfcamspo@gmail.com>
2023-05-21 10:14:27 -0700

    Add style for ComentList component

--- src/components/CommentList/CommentList.jsx
+++ src/components/CommentList/CommentList.jsx
@@ -3,26 +3,25 @@ import "./CommentList.scss";
 
 function CommentList({ activeComments, ReactTimeAgo, avatarImg, Avatar }) {
   return (
-    <ul className="comment__messages-list">
+    <ul className="comment__list">
       {activeComments
         .sort((a, b) => {
           return b.timestamp - a.timestamp;
         })
         .map((comment) => (
-          <li
-            key={comment.id}
-            className="comment__container comment__container--messages"
-          >
-            <Avatar classes="avatar avatar--comments" />
+          <li key={comment.id} className="comment__list-item">
+            <div class="comment__list-item-container">
+              <Avatar classes="avatar avatar--comments" />
 
-            <div className="comment__message">
-              <div className="comment__message-header">
-                <p className="comment__title">{comment.name}</p>
-                <p className="comment__date">
-                  <HumanTimeAgo timestampdate={comment.timestamp} />
-                </p>
+              <div className="comment__content">
+                <div className="comment__header">
+                  <p className="comment__title">{comment.name}</p>
+                  <p className="comment__date">
+                    <HumanTimeAgo timestampdate={comment.timestamp} />
+                  </p>
+                </div>
+                <p className="comment__text">{comment.comment}</p>
               </div>
-              <p className="comment__text">{comment.comment}</p>
             </div>
           </li>
         ))}
