9bfaa2ae0df74fad1eb76bc131c8dff1a17e11e4
Marcelo <mjfcamspo@gmail.com>
2023-06-01 14:36:07 -0700

    add window.scrollTo the top when select a video from the list

--- src/components/VideoList/VideoList.jsx
+++ src/components/VideoList/VideoList.jsx
@@ -7,6 +7,11 @@ function VideoList({ videosList, activeVideo }) {
 
   const handleVideoClick = (id) => {
     navigate(`/video/${id}`);
+    window.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: "smooth",
+    });
   };
 
   return (
